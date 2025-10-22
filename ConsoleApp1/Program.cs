041432aeccc6eaed8215d80fc370418d9c1f1743
Illia <zinukila@gmail.com>
2025-10-22 11:17:21 +0200

    mini commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
-﻿Console.WriteLine("Hello");
-string text;
-text = string.Empty;
-text = Console.ReadLine();
-Console.WriteLine("Your value: {0}", text);
+﻿int age = 0;
+Console.WriteLine("Wie alt bist du?");
+age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Your age is: {0}", age);
 
+age += 4;
+Console.WriteLine(age);
