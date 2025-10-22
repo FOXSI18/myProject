1e3584ee1e05db09efa2529b5dca66e8155bb35a
Illia <zinukila@gmail.com>
2025-10-22 08:36:04 +0200

    3 commit
    Added block "if"

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("2 + 2 = 5");
+            Console.WriteLine(2 * 6);
+
+            var a = 5;
+            if (a == 5)
+            {
+                Console.WriteLine("If block");
+            }
         }
     }
 }
