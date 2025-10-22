a6fc6c286ab3cda1bbab1446760844884c21a88c
Illia <zinukila@gmail.com>
2025-10-22 14:01:07 +0200

    My 4. mini-App 1.1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,17 +1,17 @@
-﻿int age = 0;
+﻿byte age = 0;
 
 Console.WriteLine("Print your age");
-age = Convert.ToInt32(Console.ReadLine());
+age = Convert.ToByte(Console.ReadLine());
 
 if (age < 10)
 {
     Console.WriteLine("Baby");
 }
-else if (age >= 10 && age < 20)
+else if (age < 20)
 {
     Console.WriteLine("Jugendlich");
 }
-else if (age >= 20 && age < 50)
+else if (age < 50)
 {
     Console.WriteLine("Erwachsene");
 }
