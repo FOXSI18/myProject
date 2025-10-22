661d90aee69c89aa06f1c2daa0ba1364711d1483
Illia <zinukila@gmail.com>
2025-10-22 13:43:00 +0200

    My 3. mini-App 1.1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,15 @@
 ﻿int num = 0;
 
-Console.WriteLine("Schreib dein Zahl");
-num = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Schreib dein Zahl");
+//num = Convert.ToInt32(Console.ReadLine());
 
-if (num % 2 == 0)
-{
-    Console.WriteLine("Zahl ist durch 2 teilbar");
-}
-else
-{
-    Console.WriteLine("Zahl ist NICHT durch 2 teilbar");
-}
+//if (num % 2 == 0)
+//{
+//    Console.WriteLine("Zahl ist durch 2 teilbar");
+//}
+//else
+//{
+//    Console.WriteLine("Zahl ist NICHT durch 2 teilbar");
+//}
+
+Console.WriteLine("Zahl {0} ist durch {1} 2 teilbar", num, num % 2 == 0 ? "" : "NICHT");
