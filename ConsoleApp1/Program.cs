2e9a1a35c8af9ca2696c7f12dca0d901bcf55bc4
Illia <zinukila@gmail.com>
2025-10-22 16:45:01 +0200

    My 8. mini-App 1.1 - Switch Calculator with 2 numbers

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,18 +1,15 @@
-﻿using System.Dynamic;
-using System.Threading.Channels;
-
-Console.WriteLine("Enter first number");
+﻿Console.WriteLine("Enter first number");
 int num1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Enter second number");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Enter operation (+, -, * or /)");
-char operation = Convert.ToChar(Console.ReadLine());
+char opr = Convert.ToChar(Console.ReadLine());
 
 int result = 0;
 
-switch (operation)
+switch (opr)
 {
     case '+': result = num1 + num2; break;
     case '-': result = num1 - num2; break;
@@ -20,7 +17,7 @@ switch (operation)
     case '/': 
         {
             result = num1 / num2; 
-            Console.WriteLine("Rest von dividierung: {0}", num1 % num2); 
+            Console.WriteLine("Rest von Division: {0}", num1 % num2); 
             break;
         } 
     default: Console.WriteLine("Unbekannt"); break;
