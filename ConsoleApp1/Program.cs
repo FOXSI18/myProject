aac706c3dc1979bcc6e7a042e84aa570dcc8d484
Illia <zinukila@gmail.com>
2025-10-22 15:27:39 +0200

    Revert "My 6. mini-App"
    This reverts commit 0f4f5176c34ad4f56e4934d4dc8f4ae13c0691de.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,12 +2,18 @@
 int num = Convert.ToInt32(Console.ReadLine());
 
 if (num % 3 == 0 && num % 5 == 0)
+{
     Console.WriteLine("FIZZBUZZ");
+}
 else if (num % 3 == 0)
+{
     Console.WriteLine("FIZZ");
+}
 else if (num % 5 == 0)
+{
     Console.WriteLine("BUZZ");
+}
 else
+{
     Console.WriteLine(num);
-
- 
+}
