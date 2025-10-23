cfc9683c1f428dfaf3a3f70c54ce1fc4d44cb1d6
Illia <zinukila@gmail.com>
2025-10-23 10:09:05 +0200

    N10 While

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,30 +1,53 @@
-﻿int x = 0;
-while (x < 10) // Make while condition == true
+﻿Console.WriteLine("Print dein Zahl");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+
+while (i <= num) // Make while condition == true
 {
-    Console.WriteLine(x);
-    x++;
+    if (i % 2 == 0)
+        Console.WriteLine(">>> {0} ist durch 2 teilbar.", i);
+    else
+        Console.WriteLine(i);
+
+    i++;
 }
 
 
-
-int y = 100;
-do // Start minimum 1 time
-{
-    Console.WriteLine(y);
-    y++;
-}
-while (y < 110);
+ 
 
 
 
-for (int z = 40; z < 50; z++) // Initialisierung of variable - Condition - Change value of variable
-{
-    Console.WriteLine(z);
-}
 
 
-string[] colors = { "red", "yellow", "orange", "green", "blue", "purple" };
-foreach (string color in colors)
-{
-    Console.WriteLine(color);
-}
+
+
+
+
+
+
+
+
+
+
+
+//int y = 100;
+//do // Start minimum 1 time
+//{
+//    Console.WriteLine(y);
+//    y++;
+//}
+//while (y < 110);
+
+
+
+//for (int z = 40; z < 50; z++) // Initialisierung of variable - Condition - Change value of variable
+//{
+//    Console.WriteLine(z);
+//}
+
+
+//string[] colors = { "red", "yellow", "orange", "green", "blue", "purple" }; // Array initialisierung
+//foreach (string color in colors) // Variable init, that 1 color had (in every iteration 1 element of array)
+//{
+//    Console.WriteLine(color);
+//}
