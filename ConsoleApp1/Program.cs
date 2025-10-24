f874b4903403571d71c97acc3ea4e1e5a450580e
Illia <zinukila@gmail.com>
2025-10-24 09:08:23 +0200

    24.10.25 Aufgabe 1: FIZZBUZZ

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,22 @@
-﻿for (int i = 1; i < 100; i *= 2)
+﻿for(int i = 0; i < 100; i++)
 {
-    if (i < 50)
-        continue;
-
-    Console.WriteLine(i);
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("{0} FIZZBUZZ", i);
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("{0} FIZZ", i);
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("{0} BUZZ", i);
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
 }
+
+
+
